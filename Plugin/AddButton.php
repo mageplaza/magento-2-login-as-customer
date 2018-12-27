15ce141dc97b8abc9fc262eaa83ecc34016b16b1
haitv282 <haitv282@gmail.com>
2018-12-27 18:45:29 +0700

    compatible 2.1

---
+++ Plugin/AddButton.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_LoginAsCustomer
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+namespace Mageplaza\LoginAsCustomer\Plugin;
+
+use Mageplaza\LoginAsCustomer\Helper\Data;
+
+/**
+ * Class AddButton
+ * @package Mageplaza\LoginAsCustomer\Plugin
+ */
+class AddButton
+{
+    /**
+     * @var Data
+     */
+    protected $helper;
+
+    /**
+     * AddButton constructor.
+     *
+     * @param Data $helper
+     */
+    public function __construct(Data $helper)
+    {
+        $this->helper = $helper;
+    }
+
+    /**
+     * @param \Magento\Sales\Block\Adminhtml\Order\View $subject
+     * @param $layout
+     *
+     * @return array
+     */
+    public function beforeSetLayout(\Magento\Sales\Block\Adminhtml\Order\View $subject, $layout)
+    {
+        $customerId = $subject->getOrder()->getCustomerId();
+        if ($customerId && $this->helper->isAllowLogin()){
+            $subject->addButton('login_as_customer', [
+                'label' => __('Login as Customer'),
+                'class' => 'login-as-customer',
+                'on_click' => sprintf("window.open('%s');", $subject->getUrl('mploginascustomer/login/index', ['id' => $customerId]))
+            ]);
+        }
+
+        return [$layout];
+    }
+}
