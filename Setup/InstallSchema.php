fa1d2e3e8817c6668a2a993da366af5ee36820f9
datvm <datvm@mageplaza.com>
2019-12-23 09:07:41 +0700

    add db_schema.xml for Magento 2.3.x compatibility

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -21,6 +21,7 @@
 
 namespace Mageplaza\LoginAsCustomer\Setup;
 
+use Magento\Framework\App\ProductMetadataInterface;
 use Magento\Framework\DB\Ddl\Table;
 use Magento\Framework\Setup\InstallSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
@@ -28,44 +29,77 @@ use Magento\Framework\Setup\SchemaSetupInterface;
 use Zend_Db_Exception;
 
 /**
- * @codeCoverageIgnore
+ * Class InstallSchema
+ * @package Mageplaza\LoginAsCustomer\Setup
  */
 class InstallSchema implements InstallSchemaInterface
 {
     /**
-     * {@inheritdoc}
-     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     * @var ProductMetadataInterface
+     */
+    private $productMetadata;
+
+    /**
+     * InstallSchema constructor.
+     *
+     * @param ProductMetadataInterface $productMetadata
+     */
+    public function __construct(ProductMetadataInterface $productMetadata)
+    {
+        $this->productMetadata = $productMetadata;
+    }
+
+    /**
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     *
      * @throws Zend_Db_Exception
      */
     public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
-        $installer = $setup;
+        if (version_compare($this->productMetadata->getVersion(), '2.3.0', '>=')) {
+            return;
+        }
 
-        $installer->startSetup();
+        $setup->startSetup();
 
         /**
          * Create table 'mp_login_as_customer'
          */
-        $table = $installer->getConnection()
-            ->newTable($installer->getTable('mp_login_as_customer'))
+        $table = $setup->getConnection()
+            ->newTable($setup->getTable('mp_login_as_customer'))
             ->addColumn('log_id', Table::TYPE_INTEGER, null, [
                 'identity' => true,
                 'unsigned' => true,
                 'nullable' => false,
                 'primary'  => true
             ], 'Entity ID')
-            ->addColumn('admin_id', Table::TYPE_INTEGER, null, ['unsigned' => true, 'nullable' => false, 'default' => '0'], 'Admin ID')
+            ->addColumn('admin_id', Table::TYPE_INTEGER, null, [
+                'unsigned' => true,
+                'nullable' => false,
+                'default'  => '0'
+            ], 'Admin ID')
             ->addColumn('admin_email', Table::TYPE_TEXT, 255, [], 'Admin Email')
             ->addColumn('admin_name', Table::TYPE_TEXT, 255, [], 'Admin Name')
-            ->addColumn('customer_id', Table::TYPE_INTEGER, null, ['unsigned' => true, 'nullable' => false, 'default' => '0'], 'Customer ID')
+            ->addColumn('customer_id', Table::TYPE_INTEGER, null, [
+                'unsigned' => true,
+                'nullable' => false,
+                'default'  => '0'
+            ], 'Customer ID')
             ->addColumn('customer_email', Table::TYPE_TEXT, 255, [], 'Customer Email')
             ->addColumn('customer_name', Table::TYPE_TEXT, 255, [], 'Customer Name')
             ->addColumn('token', Table::TYPE_TEXT, 64, [], 'Token')
-            ->addColumn('is_logged_in', Table::TYPE_SMALLINT, null, ['nullable' => false, 'default' => '0'], 'Is Logged In')
-            ->addColumn('created_at', Table::TYPE_TIMESTAMP, null, ['nullable' => false, 'default' => Table::TIMESTAMP_INIT], 'Creation Time')
+            ->addColumn('is_logged_in', Table::TYPE_SMALLINT, null, [
+                'nullable' => false,
+                'default'  => '0'
+            ], 'Is Logged In')
+            ->addColumn('created_at', Table::TYPE_TIMESTAMP, null, [
+                'nullable' => false,
+                'default'  => Table::TIMESTAMP_INIT
+            ], 'Creation Time')
             ->setComment('Login As Customer Logs table');
-        $installer->getConnection()->createTable($table);
+        $setup->getConnection()->createTable($table);
 
-        $installer->endSetup();
+        $setup->endSetup();
     }
 }
